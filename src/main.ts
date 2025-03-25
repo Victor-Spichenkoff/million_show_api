99d14a06f4794a0d937112af40a0c20f60a92bd1
Victor-Spichenkoff <victorspich13@gmail.com>
2025-03-25 19:29:19 -0300

    Initial create

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as process from 'node:process';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  const port = process.env.PORT ?? 3000
+
+  await app.listen(port)
+
+  console.log("Rodando em: http://localhost:" + port)
+}
+
+bootstrap()
