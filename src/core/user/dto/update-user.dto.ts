1d19fcb55f03a90d182a1fa7f37f4ae89369f4f1
Victor-Spichenkoff <victorspich13@gmail.com>
2025-07-28 18:19:41 -0300

    Fix min length validation

--- src/core/user/dto/update-user.dto.ts
+++ src/core/user/dto/update-user.dto.ts
@@ -1,18 +1,21 @@
 import {ApiProperty, PartialType} from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import {IsInt, isInt, IsOptional, IsString, MaxLength, MinLength} from "class-validator";
+import {IsOptional, IsString, MaxLength, MinLength} from "class-validator";
+import {Transform} from "class-transformer";
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
     @ApiProperty()
-    @IsString()
     @IsOptional()
+    @IsString()
+    @Transform(({ value }) => value === '' ? undefined : value)
     @MinLength(4, { message: 'Current password must be at least 4' })
     @MaxLength(12, { message: 'Current password must be lower than 12 characters' })
     currentPassword: string
 
     @ApiProperty()
-    @IsString()
     @IsOptional()
+    @IsString()
+    @Transform(({ value }) => value === '' ? undefined : value)
     @MinLength(4, { message: 'Password must be at least 4' })
     @MaxLength(12, { message: 'Password must be lower than 12 characters' })
     newPassword: string
