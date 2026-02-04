95c1dc18d546a7948d67700787052484b323ead2
Victor-Spichenkoff <victorspich13@gmail.com>
2026-02-04 18:25:05 -0300

    Fix dto validation message

--- src/core/question/dto/create-question.dto.ts
+++ src/core/question/dto/create-question.dto.ts
@@ -8,39 +8,39 @@ export class CreateQuestionDto {
 
   @ApiProperty()
   @IsString()
-  @MinLength(5, { message: "Minimum of 5" })
-  @MaxLength(100, { message: "Max of 100 characters" })
+  @MinLength(5, { message: "Label must be at least 5 characters long" })
+  @MaxLength(100, { message: "Label can't have more than 100 characters" })
   label: string
 
   @ApiProperty()
   @IsString()
-  @MinLength(1, { message: "Minimum of 1" })
-  @MaxLength(40, { message: "Max of 40 characters" })
+  @MinLength(1, { message: "Option 1 must have at least 1 character long" })
+  @MaxLength(40, { message: "Option 1 can't be more than 40 characters long" })
   option1: string
 
   @ApiProperty()
   @IsString()
-  @MinLength(1, { message: "Minimum of 1" })
-  @MaxLength(40, { message: "Max of 40 characters" })
+  @MinLength(1, { message: "Option 2 must have at least 1 character long" })
+  @MaxLength(40, { message: "Option 2 can't be more than 40 characters long" })
   option2: string
 
   @ApiProperty()
   @IsString()
-  @MinLength(1, { message: "Minimum of 1" })
-  @MaxLength(40, { message: "Max of 40 characters" })
+  @MinLength(1, { message: "Option 3 must have at least 1 character long" })
+  @MaxLength(40, { message: "Option 3 can't be more than 40 characters long" })
   option3: string
 
   @ApiProperty()
   @IsString()
-  @MinLength(1, { message: "Minimum of 1" })
-  @MaxLength(40, { message: "Max of 40 characters" })
+  @MinLength(1, { message: "Option 4 must have at least 1 character long" })
+  @MaxLength(40, { message: "Option 4 can't be more than 40 characters long" })
   option4: string
 
   @ApiProperty()
-  @IsEnum([1, 2, 3, 4], { message: "Values: 1, 2, 3,4 only" })
+  @IsEnum([1, 2, 3, 4], { message: "Invalid question answer index" })
   answerIndex: 1 | 2 | 3 | 4
 
   @ApiProperty()
-  @IsEnum([1, 2, 3], { message: "Values: 1, 2, 3 only" })
+  @IsEnum([1, 2, 3], { message: "Invalid level" })
   level: 1 | 2 | 3
 }
